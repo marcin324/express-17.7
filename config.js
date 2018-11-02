b004b4fe19033b5294cb6c9f782d11fffebe906c
Marcin Olszowski <olszyna38@gmail.com>
2018-11-02 19:30:34 +0100

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '969715997532-acoo9r3krd6kfsgt8irpe7ojlm5oqt79.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'cJyUolXXrzh4k8Tn4v49o9E2',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
